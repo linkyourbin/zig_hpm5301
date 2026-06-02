4ebf8172dbc728085153d3a67bb68918844215c9
linkyourbin <ybinlew@gmail.com>
2026-06-02 20:39:55 +0800

    Mark working slow CMSIS-DAP baseline

--- flash.sh
+++ flash.sh
@@ -5,9 +5,11 @@ CHIP="HPM5301"
 PROTOCOL="jtag"
 SPEED="20000"
 ELF="zig-out/bin/zig_hpm5301_dap"
+FLASH_PROBE="0d28:0204"
 
 zig build
 
-probe-rs download --chip "$CHIP" --protocol "$PROTOCOL" --speed "$SPEED" --binary-format elf "$ELF" || \
-probe-rs download --connect-under-reset --chip "$CHIP" --protocol "$PROTOCOL" --speed 1000 --binary-format elf "$ELF"
-# probe-rs reset --chip "$CHIP" --protocol "$PROTOCOL" --speed "$SPEED"
+probe-rs download --probe "$FLASH_PROBE" --chip "$CHIP" --protocol "$PROTOCOL" --speed "$SPEED" --binary-format elf "$ELF" || \
+probe-rs download --probe "$FLASH_PROBE" --connect-under-reset --chip "$CHIP" --protocol "$PROTOCOL" --speed 1000 --binary-format elf "$ELF"
+probe-rs reset --probe "$FLASH_PROBE" --chip "$CHIP" --protocol "$PROTOCOL" --speed "$SPEED" || \
+probe-rs reset --probe "$FLASH_PROBE" --connect-under-reset --chip "$CHIP" --protocol "$PROTOCOL" --speed 1000
